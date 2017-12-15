cd70ef645b098537d1e85c0eb0f24269f27ed2ce
relaxCP3 <1069106398@qq.com>
2017-12-15 16:12:40 +0800

    sss

---
+++ src/test/java/com/wqs/root/domian/User.java
@@ -0,0 +1,41 @@
+package com.wqs.root.domian;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldIndex;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Document(indexName = "test_user_index",type = "test_user_index")
+public class User {
+    @Id
+    private String id;
+    private String name;
+    @Field(type = FieldType.String, index = FieldIndex.not_analyzed)
+    private String sex;
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
---
+++ src/test/java/com/wqs/root/repository/UserRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.wqs.root.repository;
+
+import com.wqs.root.domian.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/applicationContext.xml")
+public class UserRepositoryTest {
+    @Resource
+    private ElasticsearchTemplate elasticsearchTemplate;
+    @Resource
+    private UserRepository userRepository;
+    @Before
+    public void before(){
+        elasticsearchTemplate.deleteIndex(User.class);
+        elasticsearchTemplate.createIndex(User.class);
+        User user1=new User();
+        user1.setId("11");
+        user1.setName("张三李四王五");
+        user1.setSex("男");
+        User user2=new User();
+        user2.setId("11");
+        user2.setName("李四");
+        user2.setSex("男");
+        userRepository.save(user1);
+        userRepository.save(user2);
+        userRepository.findByName("张三李四王五", new PageRequest(0, 10));
+    }
+    @Test
+    public void findOne(){
+        List<User> users=userRepository.findUsersByNameLike("张四");
+        System.out.println(users.size());
+    }
+    @Test
+    public void save(){
+        User user1=new User();
+        user1.setId("11");
+        user1.setName("张三");
+        user1.setSex("男");
+        User user2=new User();
+        user2.setId("11");
+        user2.setName("李四");
+        user2.setSex("男");
+        userRepository.save(user1);
+        userRepository.findByName("张三", new PageRequest(0, 10));
+    }
+}
